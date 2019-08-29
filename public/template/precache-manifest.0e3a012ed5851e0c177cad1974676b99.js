334611529d26da3cefe1ee7421b24ea4bc0030af
mochangba <wanchangba@foxmail.com>
2019-08-29 12:13:01 +0800

    shang

---
+++ public/template/precache-manifest.0e3a012ed5851e0c177cad1974676b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c9055d03b9742301ec99dc3b474291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3c74ce6509e613dbb4c",
+    "url": "/static/css/2.432db2e7.chunk.css"
+  },
+  {
+    "revision": "be547c934654ebf27cfc",
+    "url": "/static/css/main.afa16072.chunk.css"
+  },
+  {
+    "revision": "d3c74ce6509e613dbb4c",
+    "url": "/static/js/2.9da6a52a.chunk.js"
+  },
+  {
+    "revision": "be547c934654ebf27cfc",
+    "url": "/static/js/main.11296fe4.chunk.js"
+  },
+  {
+    "revision": "1b13caa2e2fa6974cb20",
+    "url": "/static/js/runtime~main.04a36a75.js"
+  }
+]);
